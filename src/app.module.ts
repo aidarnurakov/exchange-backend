509b6e0635d76515c27b1c3fd612ad61f45516d4
amaterasu-infg <foreverwakandaa@gmail.com>
2024-11-30 22:43:38 +0600

    feat(blockchain): new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { DatabaseModule } from './modules/database/database.module';
 import { UserModule } from './modules/user/user.module';
 import { OrderModule } from './modules/order/orders.module';
 import { ConfigModule } from '@nestjs/config';
+import { BlockchainModule } from './modules/blockchain/blockchain.module';
 import postgresConfig from './configs/postgres.config';
 
 @Module({
@@ -14,6 +15,7 @@ import postgresConfig from './configs/postgres.config';
     OrderModule,
     DatabaseModule,
     UserModule,
+    BlockchainModule,
   ],
   controllers: [],
   providers: [],
